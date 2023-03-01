c6284bf02902c4fae0c118d7ce114f14d1e39e75
Harish0411 <hariharanramasamy98@gmail.com>
2023-03-01 10:09:26 +0530

    print rectangle triangle

---
+++ main.js
@@ -0,0 +1,9 @@
+let a = Number(prompt("Enter row/column to print rectangle triangle"));
+
+for(i=1; i<=a; i++){
+    for(j=1; j<=i; j++){
+	    	
+		   document.write("*" + " ");
+}
+	document.write("<br>");
+}
